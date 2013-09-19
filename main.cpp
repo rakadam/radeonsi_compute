aa7e36fddd7a6158002e14053244372f0c82ddee
Adam Rak <adam.rak@streamnovation.com>
2013-09-20 01:42:03 +0200

    TRAN_EN bit found, and settable

--- main.cpp
+++ main.cpp
@@ -759,7 +759,7 @@ rak_adam: 0x48 is the TC (texture cache)
   state.priority = 0;
   state.debug_mode = 0;
 	state.priv_mode = 0;
-	state.trap_en = 1;
+	state.trap_en = 0;
   state.ieee_mode = 0;
   state.scratch_en = 0;
   state.lds_size = 128; ///32K
