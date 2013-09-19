a97e4440839bcb41cb407f65bb22788af29e952b
Adam Rak <adam.rak@streamnovation.com>
2013-09-19 23:03:11 +0200

    *auto ...

--- main.cpp
+++ main.cpp
@@ -749,7 +749,7 @@ rak_adam: 0x48 is the TC (texture cache)
   state.start[0] = 0;
   state.start[1] = 0;
   state.start[2] = 0;
-  state.num_thread[0] = 64*4*2;
+  state.num_thread[0] = 64*4*4;
   state.num_thread[1] = 1;
   state.num_thread[2] = 1;
   
