9e1a509cde5636973d955cfef451c121903500d7
Adam Rak <adam.rak@streamnovation.com>
2013-09-19 23:06:31 +0200

    *auto ...

--- main.cpp
+++ main.cpp
@@ -648,7 +648,7 @@ rak_adam: 0x48 is the TC (texture cache)
   {
 		
    s_mov_b32(p, 6, 6);
-   s_mov_b32(p, 6, 6);
+   s_mov_b32(p, 7, 7);
 //    v_sin_f32(p, 4, 256+4);
       v_add_f32(p, 4, 4, 256+4);
 //    v_add_f32_imm32(p, 4, 4, 1.1);
