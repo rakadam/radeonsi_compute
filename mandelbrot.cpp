4e86ddb6fb50d4adbab8e7595af20ccf0b679f74
Adam Rak <adam.rak@streamnovation.com>
2013-09-22 00:25:26 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -82,12 +82,10 @@ void set_program(unsigned* p, int mx, int my)
 	
 	s_nop(p);
 	
-	v_add_f32(p, 10, 10, 243); //v10 = v10 + 1;
+	v_add_f32(p, 10, 10, 242); //v10 = v10 + 1;
 	s_add_i32(p, 8, 8, 129); //s8 = s8 + 1;
 	
- 	v_cmpx_lt_f32(p, 10, 255); p[0]=floatconv(-7.0); p++;
-	
-// 	s_and_b64(p, 126, 126, 106); //EXEC &= VCC
+	v_cmpx_lt_f32(p, 10, 255); p[0]=floatconv(-7.0); p++;
 	
 	s_cbranch_execz(p, 3);//Exit loop if vector unit is idle
 	s_cmp_lt_i32(p, 8, 255); p[0] = 100; p++;
