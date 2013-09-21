97efb17dbacf035e166f2e51b7eb5a2bc48dbc3a
Adam Rak <adam.rak@streamnovation.com>
2013-09-22 00:20:55 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -82,7 +82,7 @@ void set_program(unsigned* p, int mx, int my)
 	
 	s_nop(p);
 	
-	v_add_f32(p, 10, 10, 242); //v10 = v10 + 1;
+	v_add_f32(p, 10, 10, 243); //v10 = v10 + 1;
 	s_add_i32(p, 8, 8, 129); //s8 = s8 + 1;
 	
  	v_cmpx_lt_f32(p, 10, 255); p[0]=floatconv(-7.0); p++;
