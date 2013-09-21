d75deb65593de0afc60590badba6e7bd8287bc22
Adam Rak <adam.rak@streamnovation.com>
2013-09-22 01:53:32 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -137,8 +137,8 @@ void set_program(unsigned* p, int mx, int my)
 
 int main()
 {
-	int mx = 256;
-	int my = 256;
+	int mx = 1024;
+	int my = 1024;
 	
 	ComputeInterface compute("/dev/dri/card0");
 	
