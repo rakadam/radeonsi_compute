78eafaf46415a87c16cd7734b21b29a701d01af1
Adam Rak <adam.rak@streamnovation.com>
2013-09-21 23:06:10 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -78,7 +78,7 @@ void set_program(unsigned* p, int mx, int my)
 	s_mov_imm32(p, 8, 0); //s8 = 0;
 	
 	unsigned* eleje = p; //eleje:
-// 	s_nop(p);
+	s_nop(p);
 	
 	s_add_i32(p, 8, 8, 129); //s8 = s8 + 1;
 	
@@ -87,7 +87,7 @@ void set_program(unsigned* p, int mx, int my)
 	
 	s_cbranch_execz(p, 3);
 	s_cmp_lt_i32(p, 8, 255); p[0] = 100; p++;
-	s_cbranch_scc0(p, eleje-p-4); //if (s8 <= 100) goto eleje;
+	s_cbranch_scc0(p, eleje-p); //if (s8 <= 100) goto eleje;
 	
 	
  	//v_mul_lo_i32(p, 10, 256+10, 256+10);
