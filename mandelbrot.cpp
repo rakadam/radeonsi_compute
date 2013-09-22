c511a49ac71ace2da93cc23fd2c7d3bc786cd479
Adam Rak <adam.rak@streamnovation.com>
2013-09-22 02:02:21 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -99,7 +99,7 @@ void set_program(unsigned* p, int mx, int my)
 	
 	s_cbranch_execz(p, 3);//Exit loop if vector unit is idle
 	
-	s_cmp_lt_i32(p, 8, 255); p[0] = 0xFFFFFF; p++;
+	s_cmp_lt_i32(p, 8, 255); p[0] = 0xFFF; p++;
 	s_cbranch_scc0(p, eleje-p-1); //if (s8 <= 100) goto eleje;
 	
 	s_mov_b64(p, 126, 12); //restore exec from s12-s13
