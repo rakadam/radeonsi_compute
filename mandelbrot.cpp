4aea8ecf97af30b3317bc2208f5fb0cb8152b76d
Adam Rak <adam.rak@streamnovation.com>
2013-09-22 02:10:18 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -78,7 +78,7 @@ void set_program(unsigned* p, int mx, int my)
 	s_mov_b64(p, 12, 126); //SAVE exec to s12-s13
 	s_mov_imm32(p, 8, 0); //s8 = 0;
 	v_mov_imm32(p, 10, floatconv(0));
-	v_mov_imm32(p, 12, floatconv(0));
+	v_mov_imm32(p, 12, floatconv(0.2));
 	
 	unsigned* eleje = p; //start label
 	
@@ -148,8 +148,8 @@ int64_t get_time_usec()
 
 int main()
 {
-	int mx = 1024*4;
-	int my = 1024*4;
+	int mx = 1024*1;
+	int my = 1024*1;
 	
 	ComputeInterface compute("/dev/dri/card0");
 	
