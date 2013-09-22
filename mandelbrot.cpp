3c1b5efeff52662b435797587426aa007d49b5bd
Adam Rak <adam.rak@streamnovation.com>
2013-09-22 02:11:50 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -78,7 +78,7 @@ void set_program(unsigned* p, int mx, int my)
 	s_mov_b64(p, 12, 126); //SAVE exec to s12-s13
 	s_mov_imm32(p, 8, 0); //s8 = 0;
 	v_mov_imm32(p, 10, floatconv(0));
-	v_mov_imm32(p, 12, floatconv(0.2));
+	v_mov_imm32(p, 12, floatconv(-0.2));
 	
 	unsigned* eleje = p; //start label
 	
