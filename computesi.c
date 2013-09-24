f4a103e9f7d2627bb13c3256f9198ff658bf35b0
Adam Rak <adam.rak@streamnovation.com>
2013-09-24 06:50:51 +0200

    *auto ...

--- computesi.c
+++ computesi.c
@@ -511,6 +511,8 @@ int compute_send_dma_fence(struct compute_context* ctx, struct gpu_buffer* bo)
 	
 	int r = drmCommandWriteRead(ctx->fd, DRM_RADEON_CS, &cs, sizeof(struct drm_radeon_cs));
 	
+	printf("%s\n", strerror(-r));
+	
 // 	compute_bo_wait(bo);
 	
 	return r;
