44e4daf55d2bf78a3dbe8ec0e50b929d2480244f
Adam Rak <adam.rak@streamnovation.com>
2013-09-25 01:38:17 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -84,7 +84,7 @@ void imageToFrameBuffer(ComputeInterface& compute, gpu_buffer* buffer, int mx, i
 		return;
 	}
 	
-	size_t size = 1280*1024;
+	size_t size = 1024*1024*4;
 	
 	p = (char*)mmap (0, size, PROT_WRITE, MAP_SHARED, fd, 0);
 	
