8d9947dfa929f91df96504bd77a03fb286e98bd7
Adam Rak <adam.rak@streamnovation.com>
2013-09-25 01:45:41 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -281,8 +281,8 @@ void set_program(unsigned* p, int mx, int my, double image_scale=1.0, double off
 
 int main()
 {
-	int mx = 1080;
-	int my = 1792;
+	int mx = 1024;
+	int my = 1920;
 	
 	
 	ComputeInterface compute("/dev/dri/card0");
