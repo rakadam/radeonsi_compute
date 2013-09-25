d547cfe5ed733deb7bfa1bc94c5f0c5949d7953e
Adam Rak <adam.rak@streamnovation.com>
2013-09-25 02:07:38 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -333,7 +333,7 @@ void animationZoom(double offset_x, double offset_y, double zoom_step, int itern
 int main()
 {
 	
-	animationZoom(0.295, 0.4507, 1.1, 200);
+	animationZoom(0.295, 0.4507005, 1.1, 200);
 	
 	return 0;
 	int mx = 1920;
