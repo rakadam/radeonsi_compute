4c45e59cb98593445e4642bd68f83f0c7094217f
Adam Rak <adam.rak@streamnovation.com>
2013-09-25 02:01:16 +0200

    *auto ...

--- mandelbrot.cpp
+++ mandelbrot.cpp
@@ -142,12 +142,11 @@ void set_program(unsigned* p, int mx, int my, double image_scale=1.0, double off
 	v_add_f32(p, 6, 6, 255); p[0] = floatconv(-mx/2); p++;
 	v_add_f32(p, 8, 8, 255); p[0] = floatconv(-my/2); p++;
 	
-	v_add_f32(p, 6, 6, 255); p[0] = floatconv((offset_x) / double(mx/2)); p++;
-	v_add_f32(p, 8, 8, 255); p[0] = floatconv(offset_y / double(my/2)); p++;
-	
 	v_mul_f32(p, 6, 6, 255); p[0] = floatconv(1.0/float(mx/2)/image_scale); p++;
 	v_mul_f32(p, 8, 8, 255); p[0] = floatconv(1.0/float(my/2)/image_scale); p++;
 	
+	v_add_f32(p, 6, 6, 255); p[0] = floatconv(offset_x); p++;
+	v_add_f32(p, 8, 8, 255); p[0] = floatconv(offset_y); p++;
 	
 	//////////////////////////////////////////////////////////////////////////////
 	///x:v6 y:v8 float32
@@ -334,7 +333,7 @@ void animationZoom(double offset_x, double offset_y, double zoom_step, int itern
 int main()
 {
 	
-	animationZoom(500, 600, 0.02, 200);
+	animationZoom(0.3, 0.1, 0.02, 200);
 	
 	return 0;
 	int mx = 1920;
